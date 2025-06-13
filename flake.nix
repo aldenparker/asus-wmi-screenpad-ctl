f1fc435c70130f02fe77794ab3d8c88595745069
Alden Parker <ajparker1401@gmail.com>
2025-06-13 16:10:52 -0700

    fix flake.nix to properly build. Time for new release

--- flake.nix
+++ flake.nix
@@ -2,21 +2,30 @@
   description = "A helper program for the asus-wmi-screenpad kernel module for linux.";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
 
   outputs =
-    { pkgs, flake-utils, ... }:
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     (flake-utils.lib.eachDefaultSystem (
       system:
-      pkgs.rustPlatform.buildRustPackage {
-        pname = "asus-wmi-screenpad-ctl";
-        version = "1.0.0";
+      let
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
 
-        src = ./.;
-
-        useFetchCargoVendor = true;
-        cargoHash = "";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
       }
     ));
 }
